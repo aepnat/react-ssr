de124e15d87bd61ff39d5ad7f48002273403defc
Saepul <aep.nat@gmail.com>
2017-11-30 00:32:03 +0700

    baseurl from config

--- server/src/helpers/createStore.js
+++ server/src/helpers/createStore.js
@@ -2,10 +2,11 @@ import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import axios from 'axios'
 import reducers from '../client/reducers'
+import config from '../../config'
 
 export default (req) => {
   const axiosInstance = axios.create({
-    baseURL: 'http://react-ssr-api.herokuapp.com',
+    baseURL: config.api_url,
     headers: { cookie: req.get('cookie') | '' }
   })
   
